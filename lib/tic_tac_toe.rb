6d6caebbaf989357d801ce2806d1f65868b6fa14
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-18 08:29:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,8 +46,8 @@ class TicTacToe
   
   def turn
     puts "Please enter 1-9:"
-    index = gets.strip
-    input = input_to_index(input)
+    input = gets.strip
+    index = input_to_index(input)
     
     if valid_move?(index)
       move(index, player_token)
